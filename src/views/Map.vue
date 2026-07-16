aef100f4ea027b8f569bffa8df0ee947e90f3f0c
한성훈 <bss445@naver.com>
2026-07-16 11:18:32 +0900

    지도 마커 이미지 오류 수정

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,4 +1,18 @@
 <script setup>
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+})
+
 import { computed, nextTick, onBeforeUnmount, onMounted, ref, watch } from 'vue'
 import L from 'leaflet'
 import { useLocalHub } from '../stores/localhub'
